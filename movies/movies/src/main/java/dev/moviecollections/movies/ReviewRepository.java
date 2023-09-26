00b913a814c723ba4af00686404a5e5fa1bc2a1b
Rusching <2785442070@qq.com>
2023-09-25 23:28:21 -0500

    init

---
+++ movies/movies/src/main/java/dev/moviecollections/movies/ReviewRepository.java
@@ -0,0 +1,10 @@
+package dev.moviecollections.movies;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, Object> {
+
+
+}
